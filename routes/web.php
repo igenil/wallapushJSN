c61b81c1b4cfcc3a553b0e74d9ce50c8f1509d86
igenil <43435719+igenil@users.noreply.github.com>
2019-01-23 10:17:39 +0100

    rutas y verification controller

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
