117ca6612576750aa14a5282aa5ec87b311428e5
igenil <43435719+igenil@users.noreply.github.com>
2019-01-30 10:42:56 +0100

    anuncios en categoria con grid

--- resources/views/anuncio/anunciosCategoria.blade.php
+++ resources/views/anuncio/anunciosCategoria.blade.php
@@ -5,25 +5,28 @@
 <br>
 <div class="container">
     <center>
-        @foreach($anuncios as $anuncio)
-            <div class="card border-dark mb-3" style="width: 6cm;">
-                <h5 class="card-title bg-dark" style="color: white">{{$anuncio->producto}}</h5>
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h3 class="card-text">{{$anuncio->precio}} €</h3>
-                    <div class="dropdown-divider"></div>
-                    <p class="card-text" style="font-size: 13px">{{$anuncio->descripcion}}</p>
-                    <div class="dropdown-divider"></div>
-                    <a href='showAnuncio/{{$anuncio->id}}'> 
-                        <button type="button" class="btn btn-outline-primary"><i class="fas fa-cart-plus"></i></button>
-                    </a>   
-                </div>
-                <div class="card-footer text-muted" style="font-size: 13px">
-                    Subido por {{$anuncio->usuario->name}}
-                </div>
+        <div class="container">
+            <div class="grid-container">
+                @foreach($anuncios as $anuncio)
+                    <div class="card border-dark mb-3" style="width: 6cm;">
+                        <h5 class="card-title bg-dark" style="color: white">{{$anuncio->producto}}</h5>
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h3 class="card-text">{{$anuncio->precio}} €</h3>
+                            <div class="dropdown-divider"></div>
+                            <p class="card-text" style="font-size: 13px">{{$anuncio->descripcion}}</p>
+                            <div class="dropdown-divider"></div>
+                            <a href='showAnuncio/{{$anuncio->id}}'> 
+                                <button type="button" class="btn btn-outline-primary"><i class="fas fa-cart-plus"></i></button>
+                            </a>   
+                        </div>
+                        <div class="card-footer text-muted" style="font-size: 13px">
+                            Subido por {{$anuncio->usuario->name}}
+                        </div>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
-
+        </div>
         <br>
     </center>
 </div>
