c69aeb5ee0836db1c206857fdab243e5731b8529
igenil <43435719+igenil@users.noreply.github.com>
2019-02-01 12:50:07 +0100

    ranking usuarios por valoraciones

--- app/Http/Controllers/TransaccionController.php
+++ app/Http/Controllers/TransaccionController.php
@@ -11,7 +11,6 @@ class TransaccionController extends Controller
 {
     public function index()
     {
-        $ventas = Anuncio::all();
         $users = User::all();
         $dinero = 0;
         $usuarios= array();
@@ -24,6 +23,23 @@ class TransaccionController extends Controller
             $dinero = 0;  
         }
         // dd($usuarios);
+        return view("transacciones.detail",compact('usuarios'));
+    }
+
+    public function index_valoraciones()
+    {
+        $users = User::all();
+        $valoracion = 0;
+        $usuarios= array();
+        foreach($users as $user){
+            $ventas = Anuncio::where('id_vendedor', $user->id)->where('vendido', 1)->get();
+            foreach($ventas as $venta){
+                $trans = Transaccion::where('id_anuncio',$venta->id)->get();
+                $valoracion += $trans->valoracion;
+            }
+            array_push($usuarios, ['name' => $user->name, 'valoracion' => $valoracion]);
+            $dinero = 0;  
+        }
         return view("transacciones.detail",compact('usuarios'));
     }
 
