f4d7981eacd37d42e38b8f647c2db293a0b65bc0
JavierPartidaMolina <38098306+JavierPartidaMolina@users.noreply.github.com>
2019-02-01 08:41:31 +0100

    transacciones sin terminar

--- app/Http/Controllers/TransaccionController.php
+++ app/Http/Controllers/TransaccionController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Anuncio;
+use App\Transaccion;
 
 class TransaccionController extends Controller
 {
-    //
+    public function index()
+    {
+        $ventas = Transaccion::all();
+        return view("transacciones.detail",compact('ventas'));
+    }
+
 }
