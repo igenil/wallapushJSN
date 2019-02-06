b6c4896cdc4f5e1824f85f9ebd486424ad993df5
igenil <43435719+igenil@users.noreply.github.com>
2019-02-06 10:47:59 +0100

    imagenes en anuncio, controlador de anuncio, directorio de imagenes y modelo de imagenes terminado

--- app/Image.php
+++ app/Image.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App;
+use App\Image;
+use App\User;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    //
+    protected $fillable = ['id', 'id_anuncio', 'img'];
+
+    public function anuncio()
+    {
+        return $this->belongsTo(Anuncio::class);
+    }
 }
