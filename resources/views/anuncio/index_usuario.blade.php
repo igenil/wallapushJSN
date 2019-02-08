ac079b2746ec888905363dae09ac764ae53632d1
igenil <43435719+igenil@users.noreply.github.com>
2019-02-08 13:43:57 +0100

    imagenes tamaño fijo

--- resources/views/anuncio/index_usuario.blade.php
+++ resources/views/anuncio/index_usuario.blade.php
@@ -20,9 +20,9 @@
         <div class="container">
             <div class="grid-container">
             @foreach($anuncios as $anuncio)
-                <div class="card border-dark mb-3" style="width: 6cm;">
+                <div class="card border-dark mb-3" style="width:250px; heigth:300px">
                     <h5 class="card-title bg-dark" style="color: white">{{$anuncio->producto}}</h5>
-                <img src="{{asset('../storage/app/public/anuncio/'. $anuncio->image[0]->img)}}" class="card-img-top" alt="...">
+                <img src="{{asset('../storage/app/public/anuncio/'. $anuncio->image[0]->img)}}" class="card-img-top" style="width:250px; heigth:300px">
                     <div class="card-body">
                         <h3 class="card-text">{{$anuncio->precio}} €</h3>
                         <div class="dropdown-divider"></div>
